a8f5f502acdf47909f0ecbe07fbd6828e10ab8a8
Maria <maria.cancian@aluno.senai.br>
2024-09-30 16:52:22 -0300

    tela inicial e responsividade

--- app/page.js
+++ app/page.js
@@ -11,10 +11,14 @@ const Selection = () => {
     <div className="h-screen bg-cover bg-no-repeat" style={{ backgroundImage: 'url("/images/telaSelection/inicioSelection.jpg")',
       backgroundPosition: 'center 56%' }}>
 
-<div>
-  <button className="bg-[#D9D9D9] text-[#9A1915] font-extrabold  py-3 px-6 border fixed bottom-14 right-16 ">ADIMINISTRADOR</button>
-
-</div>
+<div className="flex justify-end gap-4 fixed bottom-14 right-8">
+        <button className="bg-[#D9D9D9] text-[#9A1915] font-extrabold text-base sm:text-xl py-3 px-6 border">
+          ADMINISTRADOR
+        </button>
+        <button className="bg-[#D9D9D9] text-[#9A1915] xl:hidden font-extrabold text-base sm:text-xl py-3 px-6 border">
+          TOTEM
+        </button>
+      </div>
 
     </div>
   );
