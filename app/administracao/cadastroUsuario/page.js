ba2f5b136871a50c72a9619f6f76ba098a4e5932
Maria Rita Gomes <mariagomes@aluno.senai.br>
2024-09-30 16:25:37 -0300

    feat: tela selection inicio

---
+++ app/administracao/cadastroUsuario/page.js
@@ -0,0 +1,170 @@
+'use client';
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import api from '../src/config/configApi';
+import * as faceapi from 'face-api.js';
+
+const InputComponent = () => {
+  const [image, setImage] = useState(null);
+  const [nif, setNif] = useState('');
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [notification, setNotification] = useState(false);
+  const [notiwhere, setNotiwhere] = useState(0);
+  const [adm, setAdm] = useState(false);
+  const [preview, setPreview] = useState(null);
+
+  useEffect(() => {
+    Promise.all([
+      faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+      faceapi.nets.faceRecognitionNet.loadFromUri('/models')
+    ]).then(() => {
+      console.log('Models loaded');
+    }).catch((error) => {
+      console.error('Failed to load models:', error);
+    });
+  }, []);
+
+  const uploadImage = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('nif', nif);
+    formData.append('nome', nome);
+    formData.append('email', email);
+    formData.append('telefone', telefone);
+    formData.append('adm', adm);
+    formData.append('notification', notification);
+    formData.append('notiwhere', notiwhere);
+    formData.append('image', image);
+
+    try {
+      const img = await faceapi.fetchImage(preview);
+      const detection = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
+
+      if (detection) {
+        const descriptors = detection.descriptor;
+
+        const descriptor = descriptors.join(',')
+
+        formData.append('descriptor', descriptor);
+        console.log('Face descriptors:', descriptors.join(','));
+        const response = await api.post("/usuarios", formData);
+        console.log(response);
+      } else {
+        console.log('No face detected');
+      }
+    } catch (err) {
+      if (err.response) {
+        console.log(err.response);
+      } else {
+        console.log("Erro, tente novamente mais tarde.");
+      }
+    }
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+    console.log(file);
+
+    if (file) {
+      setPreview(URL.createObjectURL(file));
+    } else {
+      setPreview(null);
+    }
+  };
+
+  return (
+    <div style={styles.container}>
+      <form onSubmit={uploadImage}>
+        <label style={styles.label}>Imagem:</label>
+        <input
+          type="file"
+          name="image"
+          onChange={handleImageChange}
+          style={styles.input}
+        />
+
+        <label style={styles.label}>Nif:</label>
+        <input
+          type="number"
+          name="nif"
+          value={nif}
+          onChange={(e) => setNif(e.target.value)}
+          style={styles.input}
+        />
+
+        <label style={styles.label}>Nome:</label>
+        <input
+          type="text"
+          name="nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          style={styles.input}
+        />
+
+        <label style={styles.label}>Telefone:</label>
+        <input
+          type="number"
+          name="telefone"
+          value={telefone}
+          onChange={(e) => setTelefone(e.target.value)}
+          style={styles.input}
+        />
+
+        <label style={styles.label}>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          style={styles.input}
+        />
+
+        <button type="submit">Enviar</button>
+
+        {preview && (
+          <div style={styles.imageContainer}>
+            <Image
+              src={preview}
+              alt="Imagem pré-visualizada"
+              width={300}
+              height={300}
+              style={styles.image}
+            />
+          </div>
+        )}
+      </form>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    padding: '20px',
+  },
+  label: {
+    fontSize: '16px',
+    marginBottom: '10px',
+    display: 'block',
+  },
+  input: {
+    height: '30px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    padding: '5px 10px',
+    marginBottom: '20px',
+    width: '100%',
+    maxWidth: '300px',
+  },
+  imageContainer: {
+    marginTop: '20px',
+  },
+  image: {
+    borderRadius: '10px',
+  },
+};
+
+export default InputComponent;
