f9c2b8c187df10654ad268d6d5855ab4e82de7fe
Rafael-Cumpri <rafael.cumpri@aluno.senai.br>
2024-10-07 16:40:36 -0300

    feat : get user by nif new page

--- app/totem/paginaModal/page.jsx
+++ app/totem/paginaModal/page.jsx
@@ -1,8 +1,24 @@
 "use client"
 import BasicModal from "@/app/components/modal/modal";
 import Header from "@/app/components/header/Header";
+import { useEffect, useState } from "react";
+import api from "../../../src/config/configApi";
 
-const PaginaModal = () => {
+const PaginaModal = ({ params }) => {
+    const { nif } = params;
+    const [user, setUser] = useState({})
+
+    useEffect(() => {
+        async function fetchUser() {
+            try {
+                const response = await api.get("/usuarios/" + nif)
+                setUser(response.data)
+            } catch (error) {
+                console.error("Erro ao acessar a câmera: ", err);
+            }
+        }
+    })
+
     return(
     <div className=" bg-white h-screen">
         <Header />
