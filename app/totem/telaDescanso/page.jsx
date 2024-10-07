d1ce20172a192081283279fac04dc50ec2fb3d9f
Maria <maria.cancian@aluno.senai.br>
2024-10-07 16:27:43 -0300

    termino segunda tela de descanso

--- app/totem/telaDescanso/page.jsx
+++ app/totem/telaDescanso/page.jsx
@@ -2,20 +2,17 @@
 
 import { useRouter } from "next/navigation";
 
-
 const telaDescanso = () => {
-
   const router = useRouter();
 
-
-  const clicou= async () => {
-    router.push(`/totem/paginaModal`)
-  }
+  const clicou = async () => {
+    router.push(`/totem/paginaModal`);
+  };
 
   return (
     <div
-    onClick={clicou}
-      className="h-screen bg-cover bg-no-repeat flex flex-col items-center justify-start"
+      onClick={clicou}
+      className="h-screen bg-cover bg-no-repeat flex flex-col items-center justify-start relative"
       style={{
         backgroundImage: 'url("/images/telaDescanso/imagemDescanso.jpg")',
         backgroundPosition: "center 56%",
@@ -23,16 +20,21 @@ const telaDescanso = () => {
     >
       <img
         src="/images/logoSenai/logo.png"
-        className="w-[50%] border-[14px] border-white rounded-lg mb-12 mt-9" // Aumentei a margem superior da imagem
+        className="w-[50%] border-[14px] border-white rounded-lg mb-12 mt-9"
       />
 
-
-      
-
-      
+      {/* Seção com opacidade na parte inferior */}
+      <div
+        className="w-full h-1/3 bg-white bg-opacity-60 absolute bottom-0 flex justify-center items-center"
+      >
+        <div className="flex items-center justify-center space-x-1">
+          <p className="text-5xl text-black">Toque na</p>
+          <p className="text-5xl text-black font-bold">tela</p>
+          <p className="text-5xl text-black">para</p>
+          <p className="text-5xl text-black font-bold">iniciar</p>
+        </div>
+      </div>
     </div>
-
-
   );
 };
 
