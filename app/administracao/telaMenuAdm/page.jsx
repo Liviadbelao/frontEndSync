b758ee4f6430dd8ab5d137eb4248726e75b6c26d
Cumpri <rafaelcumpri.senai564@gmail.com>
2024-10-09 15:29:06 -0300

    Refactor TelaMenuAdm page to fetch user data and redirect to login if user is not an admin

--- app/administracao/telaMenuAdm/page.jsx
+++ app/administracao/telaMenuAdm/page.jsx
@@ -1,34 +1,75 @@
 "use client"
-import React from 'react'
-import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import TopicosMenu from '@/app/components/topicosMenu/TopicosMenu';
 import Header from '@/app/components/header/Header';
+import api from '../../../src/config/configApi';
+
 const TelaMenuAdm = () => {
     const router = useRouter();
+    const searchParams = useSearchParams();
+    const nif = searchParams.get('nif');
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchUser() {
+            try {
+                const response = await api.get(`/usuarios/${nif}`);
+                if (response.data.length > 0) {
+                    setUser(response.data[0]);
+                } else {
+                    setUser(null);
+                }
+            } catch (error) {
+                console.error("Erro ao buscar o usuário: ", error);
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        if (nif) {
+            fetchUser();
+        } else {
+            setLoading(false);
+        }
+    }, [nif]);
+
+    useEffect(() => {
+        if (!loading) {
+            if (!user || !user.adm) {
+                alert("Nenhum usuário com esse NIF encontrado, redirecionando para login.");
+                router.push('/administracao/login');
+            }
+        }
+    }, [loading, user, router]);
+
+    if (loading) {
+        return <div>Carregando...</div>;
+    }
+
     return (
-        // tela de menu do adm tela branca
-        <div className=" items-center justify-center min-h-screen ">
-            <Header />
-
-            <h1 className="font-extrabold text-2xl text-center mt-4">O que deseja fazer?</h1>
-
-            {/* menu de opções tela cinza */}
-            <div className="flex justify-center ml-40 mr-40 mt-4 mb-4 p-10 rounded-md bg-zinc-300 border-4 border-red-500">
-                {/* topicos com navegacao */}
-                <div className="grid grid-cols-2 gap-10">
-                    <TopicosMenu
-                        topicos="Monitoração de ambientes"
-                        imgSrc="/images/imgMenuAdm/monitoracao.png"
-                        onClick={() => router.push('/administracao/cadastroAmbiente')}
-                    />
-                    <TopicosMenu topicos="Gestão de  ambientes" imgSrc="/images/imgMenuAdm/gestaol.png" />
-                    <TopicosMenu topicos="Gestão de funcionários" imgSrc="/images/imgMenuAdm/gestaof.png" />
-                    <TopicosMenu topicos="Reserva de ambientes" imgSrc="/images/imgMenuAdm/reservaicon.png" />
-                    <TopicosMenu topicos="Controle de informações" imgSrc="/images/imgMenuAdm/painel.png" />
+        user && user.adm ? (
+            <div className="items-center justify-center min-h-screen">
+                <Header />
+                <h1 className="font-extrabold text-2xl text-center mt-4">O que deseja fazer?</h1>
+                <div className="flex justify-center ml-40 mr-40 mt-4 mb-4 p-10 rounded-md bg-zinc-300 border-4 border-red-500">
+                    <div className="grid grid-cols-2 gap-10">
+                        <TopicosMenu
+                            topicos="Monitoração de ambientes"
+                            imgSrc="/images/imgMenuAdm/monitoracao.png"
+                            onClick={() => router.push('/administracao/cadastroAmbiente')}
+                        />
+                        <TopicosMenu topicos="Gestão de  ambientes" imgSrc="/images/imgMenuAdm/gestaol.png" />
+                        <TopicosMenu topicos="Gestão de funcionários" imgSrc="/images/imgMenuAdm/gestaof.png" />
+                        <TopicosMenu topicos="Reserva de ambientes" imgSrc="/images/imgMenuAdm/reservaicon.png" />
+                        <TopicosMenu topicos="Controle de informações" imgSrc="/images/imgMenuAdm/painel.png" />
+                    </div>
                 </div>
             </div>
-        </div>
+        ) : null
     );
 }
 
-export default TelaMenuAdm; 
+export default TelaMenuAdm;
