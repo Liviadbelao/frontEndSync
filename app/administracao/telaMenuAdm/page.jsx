257a0950b3ca74efba76aa11538e4569be33c3c9
isabela souza <isabela.souza7@aluno.senai.br>
2024-10-09 11:25:34 -0300

    feat: Adicionar cabeçalho à página de menu do administrador
    Adiciona o componente de cabeçalho à página de menu do administrador, para melhorar a experiência do usuário e fornecer uma identificação visual clara da página.

--- app/administracao/telaMenuAdm/page.jsx
+++ app/administracao/telaMenuAdm/page.jsx
@@ -2,18 +2,19 @@
 import React from 'react'
 import { useRouter } from 'next/navigation';
 import TopicosMenu from '@/app/components/topicosMenu/TopicosMenu';
-
+import Header from '@/app/components/header/Header';
 const TelaMenuAdm = () => {
     const router = useRouter();
     return (
         // tela de menu do adm tela branca
         <div className=" items-center justify-center min-h-screen ">
+            <Header />
 
             <h1 className="font-extrabold text-2xl text-center mt-4">O que deseja fazer?</h1>
 
             {/* menu de opções tela cinza */}
-            <div className="flex justify-center ml-40 mr-40 mt-10 p-10 rounded-md bg-zinc-300 border-4 border-red-500">
-               {/* topicos com navegacao */}
+            <div className="flex justify-center ml-40 mr-40 mt-4 mb-4 p-10 rounded-md bg-zinc-300 border-4 border-red-500">
+                {/* topicos com navegacao */}
                 <div className="grid grid-cols-2 gap-10">
                     <TopicosMenu
                         topicos="Monitoração de ambientes"
