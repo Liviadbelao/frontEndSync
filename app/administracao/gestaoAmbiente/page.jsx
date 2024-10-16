f063f9b97dbdb819147257fb881f10deb6e030ec
isabela souza <125027572+isa1307@users.noreply.github.com>
2024-10-16 09:52:47 -0300

    Update page.jsx

--- app/administracao/gestaoAmbiente/page.jsx
+++ app/administracao/gestaoAmbiente/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 import React from "react";
+import { useRouter } from "next/navigation";
 import Header from "@/app/components/header/Header";
 import GestaoAmbientes from "@/app/components/gestaoAmbientes/GestaoAmbientes";
 
@@ -7,22 +8,80 @@ const handleEdit = () => {
     console.log("Editar ambiente");
 }
 
-const handleDelete = () => {
-    console.log("Deletar ambiente");
+const handleDelete = async (id) => {
+    /*   try {
+          await api.delete(`/ambientes/${id}`);
+          setAmbientes(ambientes.filter(ambiente => ambiente.id !== id));
+          console.log("Ambiente deletado com sucesso");
+      } catch (error) {
+          console.error("Erro ao deletar ambiente:", error);
+      } */
 }
 
 const GestaoAmbiente = () => {
+    const router = useRouter();
+
     return (
         <div>
             <Header />
-            <h1>Gestão de Ambientes</h1>
-            <GestaoAmbientes 
+            {/* botao para voltar para o menu */}
+            <img
+                src="/images/imgMenuAdm/botao-voltar.png"
+                alt="botao voltar"
+                className="mr-10 cursor-pointer w-10 h-10 mt-2 ml-10"
+                onClick={() => router.push("/administracao/telaMenuAdm")}
+            />
+
+            <h1 className="text-center text-3xl	font-bold mt-2 mb-6">Gestão de Ambientes</h1>
+           
+
+{/* img para add ambiente */}
+            <div className="grid lg:grid-cols-4 gap-10 ml-10">
+                <div className="flex items-center ml-10 mb-4">
+                    <img
+                        src="/images/imgMenuAdm/botao-adicionar.png"
+                        alt="oi"
+                        className="ml-30 cursor-pointer"
+                        onClick={() => router.push("/administracao/cadastroAmbiente")}
+                    />
+                </div>
+                {/* componentes */}
+                <GestaoAmbientes
                     nome="Sala de Reuniões"
-                    imgSrc="/images/imgMenuAdm/gestaof.png" 
-                    imgSrc2="/images/imgMenuAdm/senaimaquina2.jpg" 
+                    imgSrc="/images/imgMenuAdm/gestaof.png"
+                    imgSrc2="/images/imgMenuAdm/senaimaquina2.jpg"
                     onEdit={handleEdit}
                     onDelete={handleDelete}
                 />
+                <GestaoAmbientes
+                    nome="Sala de Reuniões"
+                    imgSrc="/images/imgMenuAdm/gestaof.png"
+                    imgSrc2="/images/imgMenuAdm/senaimaquina2.jpg"
+                    onEdit={handleEdit}
+                    onDelete={handleDelete}
+                />
+                <GestaoAmbientes
+                    nome="Sala de Reuniões"
+                    imgSrc="/images/imgMenuAdm/gestaof.png"
+                    imgSrc2="/images/imgMenuAdm/senaimaquina2.jpg"
+                    onEdit={handleEdit}
+                    onDelete={handleDelete}
+                />
+                <GestaoAmbientes
+                    nome="Sala de Reuniões"
+                    imgSrc="/images/imgMenuAdm/gestaof.png"
+                    imgSrc2="/images/imgMenuAdm/senaimaquina2.jpg"
+                    onEdit={handleEdit}
+                    onDelete={handleDelete}
+                />
+                <GestaoAmbientes
+                    nome="Sala de Reuniões"
+                    imgSrc="/images/imgMenuAdm/gestaof.png"
+                    imgSrc2="/images/imgMenuAdm/senaimaquina2.jpg"
+                    onEdit={handleEdit}
+                    onDelete={handleDelete}
+                />
+            </div>
         </div>
     );
 }
