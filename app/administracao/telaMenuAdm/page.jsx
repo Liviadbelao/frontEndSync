c1ec0c12830fc26c98a2a805c037bd606fc10b3f
Liviadbelao <liviabelao.564@gmail.com>
2024-10-21 11:22:53 -0300

    feat: Adicionar página de notificação
    Adiciona um novo componente de página para exibir notificações.

--- app/administracao/telaMenuAdm/page.jsx
+++ app/administracao/telaMenuAdm/page.jsx
@@ -5,29 +5,54 @@ import TopicosMenu from '@/app/components/topicosMenu/TopicosMenu';
 import Header from '@/app/components/header/Header';
 import api from '../../../src/config/configApi';
 
+const TelaMenuAdm = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const nif = searchParams.get('nif');
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        async function fetchUser() {
+            try {
+                const response = await api.get(`/usuarios/${nif}`);
+                if (response.data.length > 0) {
+                    setUser(response.data[0]);
+                } else {
+                    setUser(null);
+                }
+            } catch (error) {
+                console.error("Erro ao buscar o usuário: ", error);
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        }
 
+        if (nif) {
+            fetchUser();
+        } else {
+            setLoading(false);
+        }
+    }, [nif]);
+
+    useEffect(() => {
+        if (!loading) {
+            if (!user || !user.adm) {
+                alert("Nenhum usuário com esse NIF encontrado, redirecionando para login.");
+                router.push('/administracao/login');
+            }
+        }
+    }, [loading, user, router]);
+
+    if (loading) {
+        return <div>Carregando...</div>;
+    }
 
     return (
         user && user.adm ? (
             <div className="items-center justify-center min-h-screen">
                 <Header />
-          
-            {/* menu de opções tela cinza */}
-            <div className="flex justify-center ml-40 mr-40 mt-4 mb-4 p-10 rounded-md bg-zinc-300 border-4 border-red-500">
-                {/* topicos com navegacao */}
-                <div className="grid grid-cols-2 gap-10">
-                    <TopicosMenu
-                        topicos="Monitoração de ambientes"
-                        imgSrc="/images/imgMenuAdm/monitoracao.png"
-                    />
-                    <TopicosMenu topicos="Gestão de  ambientes" imgSrc="/images/imgMenuAdm/gestaol.png"
-                      onClick={() => router.push('/administracao/gestaoAmbiente')}
-                     />
-                    <TopicosMenu topicos="Gestão de funcionários" imgSrc="/images/imgMenuAdm/gestaof.png" />
-                    <TopicosMenu topicos="Reserva de ambientes" imgSrc="/images/imgMenuAdm/reservaicon.png" />
-                    <TopicosMenu topicos="Controle de informações" imgSrc="/images/imgMenuAdm/painel.png" />
-
                 <h1 className="font-extrabold text-2xl text-center mt-4">O que deseja fazer?</h1>
                 <div className="flex justify-center ml-40 mr-40 mt-4 mb-4 p-10 rounded-md bg-zinc-300 border-4 border-red-500">
                     <div className="grid grid-cols-2 gap-10">
@@ -41,7 +66,6 @@ import api from '../../../src/config/configApi';
                         <TopicosMenu topicos="Reserva de ambientes" imgSrc="/images/imgMenuAdm/reservaicon.png" />
                         <TopicosMenu topicos="Controle de informações" imgSrc="/images/imgMenuAdm/painel.png" />
                     </div>
-
                 </div>
             </div>
         ) : null
---
+++ app/components/notificacao/page.jsx
@@ -0,0 +1,11 @@
+const notificacao = ()=>{
+    return(
+        <div>
+            
+
+        </div>
+    )
+}
+
+export default notificacao
+
