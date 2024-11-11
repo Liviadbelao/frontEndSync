2cfbd7746b7664a09435e6835dc9d7c32b058b3c
giobasiliox <giovana.basilio@aluno.senai.br>
2024-11-11 10:51:09 -0300

    feat: tela sucesso ao enviar cadastro

--- app/administracao/cadastroAmbiente/page.jsx
+++ app/administracao/cadastroAmbiente/page.jsx
@@ -118,220 +118,224 @@ const Ambiente = () => {
             }
             limparInputs();
             setShowSuccess(true); // Exibe a TelaCertinho
-            setTimeout(() => setShowSuccess(false), 2000); // Oculta após 2 segundos
-            router.push(`/administracao/gestaoAmbiente?nif=${nif}`); // Redireciona após a exibição da TelaCertinho
-        } catch (err) {
-            if (err.response) {
-                console.log(err.response);
-            } else {
-                console.log("Erro, tente novamente mais tarde.");
-            }
+
+            setTimeout(() => {
+                setShowSuccess(false); // Oculta a TelaCertinho
+                router.push(`/administracao/gestaoAmbiente?nif=${nif}`);
+            }, 2000); // 2 segundos de atraso
+
+    } catch (err) {
+        if (err.response) {
+            console.log(err.response);
+        } else {
+            console.log("Erro, tente novamente mais tarde.");
         }
-    };
+    }
+};
 
-    const handleImageChange = (e) => {
-        const file = e.target.files[0];
-        setImagem(file);
-    };
+const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImagem(file);
+};
 
-    const limparInputs = () => {
-        setNome("");
-        setImagem("");
-        setCapacidade(0);
-        setCategoriaSelecionada("");
-        setDisponivel(true);
-        setNumeroAmbiente(1);
-        setTipodoambiente("");
-        setMaquinas(0);
-        setNumerochave(0);
-        setErrors({});
-    };
+const limparInputs = () => {
+    setNome("");
+    setImagem("");
+    setCapacidade(0);
+    setCategoriaSelecionada("");
+    setDisponivel(true);
+    setNumeroAmbiente(1);
+    setTipodoambiente("");
+    setMaquinas(0);
+    setNumerochave(0);
+    setErrors({});
+};
 
-    useEffect(() => {
-        async function fetchCategorias() {
-            try {
-                const response = await api.get("/categorias");
-                setCategorias(response.data);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
+useEffect(() => {
+    async function fetchCategorias() {
+        try {
+            const response = await api.get("/categorias");
+            setCategorias(response.data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
         }
+    }
 
-        fetchCategorias();
-    }, []);
+    fetchCategorias();
+}, []);
 
-    return (
-        <div className="bg-white min-h-screen flex flex-col overflow-y-auto ">
-            <Header />
-            {showSuccess && <TelaCertinho onClose={() => setShowSuccess(false)} />} {/* Exibe TelaCertinho */}
-            <img
-                src="/images/imgMenuAdm/btvoltar.png"
-                alt="botao voltar"
-                className="mr-10 cursor-pointer w-10 h-10 ml-10 mt-10"
-                onClick={() => router.push(`/administracao/gestaoAmbiente?nif=${nif}`)}
-            />
+return (
+    <div className="bg-white min-h-screen flex flex-col overflow-y-auto ">
+        <Header />
+        {showSuccess && <TelaCertinho onClose={() => setShowSuccess(false)} />} {/* Exibe TelaCertinho */}
+        <img
+            src="/images/imgMenuAdm/btvoltar.png"
+            alt="botao voltar"
+            className="mr-10 cursor-pointer w-10 h-10 ml-10 mt-10"
+            onClick={() => router.push(`/administracao/gestaoAmbiente?nif=${nif}`)}
+        />
 
-            <div className="flex flex-col items-center justify-center">
-                <p className="font-normal md:font-bold mt-40 text-2xl">Cadastro de Ambiente</p>
-                <form
-                    className="flex flex-col bg-[#D9D9D9] text-black w-[55%] border-2 border-red-500 items-center pt-6 mb-16 rounded-md"
-                    onSubmit={postAmbiente}
-                >
-                    <div className="w-[70%] m-2">
-                        <label>Nome do ambiente:</label>
-                        <Input
-                            tipo={"text"}
-                            placeholder={"Nome"}
-                            valor={nome}
-                            onChange={(e) => setNome(e.target.value)}
-                            nome={"nome"}
-                        />
-                        {errors.nome && <span className="text-red-500">{errors.nome}</span>}
-                    </div>
-                    <div className="w-[70%] m-2">
-                        <label>Selecione uma imagem do ambiente:</label>
-                        <Input
-                            tipo={"file"}
-                            placeholder={"Imagem"}
-                            onChange={handleImageChange}
-                            nome={"imagem"}
-                        />
-                        {errors.imagem && <span className="text-red-500">{errors.imagem}</span>}
-                    </div>
-                    <div className="w-[70%] m-2">
-                        <label>Capacidade de alunos no ambiente:</label>
-                        <Input
-                            tipo={"number"}
-                            placeholder={"Capacidade"}
-                            valor={capacidade}
-                            onChange={(e) => setCapacidade(e.target.value)}
-                            nome={"capacidade"}
-                        />
-                        {errors.capacidade && <span className="text-red-500">{errors.capacidade}</span>}
-                    </div>
-                    <div className="w-[70%] m-2">
-                        <label>Quantidade de máquinas:</label>
-                        <Input
-                            tipo={"number"}
-                            placeholder={"Maquinas"}
-                            valor={maquinas}
-                            onChange={(e) => setMaquinas(e.target.value)}
-                            nome={"maquinas"}
-                        />
-                    </div>
-                    <div className="w-[70%] m-2">
-                        <label>Número do ambiente:</label>
-                        <Input
-                            tipo={"number"}
-                            placeholder={"numeroAmbiente"}
-                            valor={numeroAmbiente}
-                            onChange={(e) => setNumeroAmbiente(e.target.value)}
-                            nome={"numeroAmbiente"}
-                        />
-                        {errors.numeroAmbiente && <span className="text-red-500">{errors.numeroAmbiente}</span>}
-                    </div>
-                    <div className="w-[70%] m-2">
-                        <label>Categoria que o ambiente pertence:</label>
-                        <select
-                            id="categoria"
-                            value={categoriaSelecionada}
-                            onChange={(e) => setCategoriaSelecionada(e.target.value)}
-                        >
-                            <option value="">Selecione uma categoria</option>
-                            {categorias.map((categoria) => (
-                                <option key={categoria.id} value={categoria.id}>
-                                    {categoria.nome}
-                                </option>
-                            ))}
-                        </select>
-                        {errors.categoriaSelecionada && <span className="text-red-500">{errors.categoriaSelecionada}</span>}
-                    </div>
-                    <div className="w-[70%] m-2">
-                        <label>Tipo do Ambiente:</label>
-                        <select
-                            id="tipodoambiente"
-                            value={tipodoambiente}
-                            onChange={(e) => setTipodoambiente(e.target.value)}
-                        >
-                            <option value="">Selecione o tipo de ambiente</option>
-                            <option value="blocooficina">Bloco Oficina</option>
-                            <option value="externo">Externo</option>
-                        </select>
-                        {errors.tipodoambiente && <span className="text-red-500">{errors.tipodoambiente}</span>}
-                    </div>
-                    <div className="w-[70%] m-2">
-                        <label>Ar condicionado:</label>
-                        <Input
-                            tipo={"checkbox"}
-                            placeholder={"Ar condicionado"}
-                            onChange={(e) => setArcondicionado(!arcondicionado)}
-                            nome={"arcondicionado"}
-                        />
-                    </div>
-                    <div className="w-[70%] m-2">
-                        <label>Wi-fi:</label>
-                        <Input
-                            tipo={"checkbox"}
-                            placeholder={"Wifi"}
-                            onChange={(e) => setWifi(!wifi)}
-                            nome={"wifi"}
-                        />
-                    </div>
-                    <div className="w-[70%] m-2">
-                        <label>Chave:</label>
-                        <Input
-                            tipo={"checkbox"}
-                            placeholder={"Chave"}
-                            onChange={(e) => setChave(!chave)}
-                            nome={"chave"}
-                        />
-                        {chave && (
-                            <>
-                                <label>Número da chave:</label>
-                                <Input
-                                    tipo={"number"}
-                                    placeholder={"N° :"}
-                                    valor={numerochave}
-                                    onChange={(e) => setNumerochave(e.target.value)}
-                                    nome={"Número"}
-                                />
-                                {errors.numerochave && <span className="text-red-500">{errors.numerochave}</span>}
-                            </>
-                        )}
-                    </div>
-                    <div className="w-[70%] m-2">
-                        <label>Projetor:</label>
-                        <Input
-                            tipo={"checkbox"}
-                            placeholder={"projetor"}
-                            onChange={(e) => setProjetor(!projetor)}
-                            nome={"projetor"}
-                        />
-                    </div>
-                    <div className="w-[70%] m-2">
-                        <label>Chave eletronica:</label>
-                        <Input
-                            tipo={"checkbox"}
-                            placeholder={"chave eletronica"}
-                            onChange={(e) => setChaveeletronica(!chaveeletronica)}
-                            nome={"chaveeletronica"}
-                        />
-                    </div>
-                    <div className="w-[70%] m-2">
-                        <label>Ventilador:</label>
-                        <Input
-                            tipo={"checkbox"}
-                            placeholder={"ventilador"}
-                            onChange={(e) => setVentilador(!ventilador)}
-                            nome={"ventilador"}
-                        />
-                    </div>
+        <div className="flex flex-col items-center justify-center">
+            <p className="font-normal md:font-bold mt-40 text-2xl">Cadastro de Ambiente</p>
+            <form
+                className="flex flex-col bg-[#D9D9D9] text-black w-[55%] border-2 border-red-500 items-center pt-6 mb-16 rounded-md"
+                onSubmit={postAmbiente}
+            >
+                <div className="w-[70%] m-2">
+                    <label>Nome do ambiente:</label>
+                    <Input
+                        tipo={"text"}
+                        placeholder={"Nome"}
+                        valor={nome}
+                        onChange={(e) => setNome(e.target.value)}
+                        nome={"nome"}
+                    />
+                    {errors.nome && <span className="text-red-500">{errors.nome}</span>}
+                </div>
+                <div className="w-[70%] m-2">
+                    <label>Selecione uma imagem do ambiente:</label>
+                    <Input
+                        tipo={"file"}
+                        placeholder={"Imagem"}
+                        onChange={handleImageChange}
+                        nome={"imagem"}
+                    />
+                    {errors.imagem && <span className="text-red-500">{errors.imagem}</span>}
+                </div>
+                <div className="w-[70%] m-2">
+                    <label>Capacidade de alunos no ambiente:</label>
+                    <Input
+                        tipo={"number"}
+                        placeholder={"Capacidade"}
+                        valor={capacidade}
+                        onChange={(e) => setCapacidade(e.target.value)}
+                        nome={"capacidade"}
+                    />
+                    {errors.capacidade && <span className="text-red-500">{errors.capacidade}</span>}
+                </div>
+                <div className="w-[70%] m-2">
+                    <label>Quantidade de máquinas:</label>
+                    <Input
+                        tipo={"number"}
+                        placeholder={"Maquinas"}
+                        valor={maquinas}
+                        onChange={(e) => setMaquinas(e.target.value)}
+                        nome={"maquinas"}
+                    />
+                </div>
+                <div className="w-[70%] m-2">
+                    <label>Número do ambiente:</label>
+                    <Input
+                        tipo={"number"}
+                        placeholder={"numeroAmbiente"}
+                        valor={numeroAmbiente}
+                        onChange={(e) => setNumeroAmbiente(e.target.value)}
+                        nome={"numeroAmbiente"}
+                    />
+                    {errors.numeroAmbiente && <span className="text-red-500">{errors.numeroAmbiente}</span>}
+                </div>
+                <div className="w-[70%] m-2">
+                    <label>Categoria que o ambiente pertence:</label>
+                    <select
+                        id="categoria"
+                        value={categoriaSelecionada}
+                        onChange={(e) => setCategoriaSelecionada(e.target.value)}
+                    >
+                        <option value="">Selecione uma categoria</option>
+                        {categorias.map((categoria) => (
+                            <option key={categoria.id} value={categoria.id}>
+                                {categoria.nome}
+                            </option>
+                        ))}
+                    </select>
+                    {errors.categoriaSelecionada && <span className="text-red-500">{errors.categoriaSelecionada}</span>}
+                </div>
+                <div className="w-[70%] m-2">
+                    <label>Tipo do Ambiente:</label>
+                    <select
+                        id="tipodoambiente"
+                        value={tipodoambiente}
+                        onChange={(e) => setTipodoambiente(e.target.value)}
+                    >
+                        <option value="">Selecione o tipo de ambiente</option>
+                        <option value="blocooficina">Bloco Oficina</option>
+                        <option value="externo">Externo</option>
+                    </select>
+                    {errors.tipodoambiente && <span className="text-red-500">{errors.tipodoambiente}</span>}
+                </div>
+                <div className="w-[70%] m-2">
+                    <label>Ar condicionado:</label>
+                    <Input
+                        tipo={"checkbox"}
+                        placeholder={"Ar condicionado"}
+                        onChange={(e) => setArcondicionado(!arcondicionado)}
+                        nome={"arcondicionado"}
+                    />
+                </div>
+                <div className="w-[70%] m-2">
+                    <label>Wi-fi:</label>
+                    <Input
+                        tipo={"checkbox"}
+                        placeholder={"Wifi"}
+                        onChange={(e) => setWifi(!wifi)}
+                        nome={"wifi"}
+                    />
+                </div>
+                <div className="w-[70%] m-2">
+                    <label>Chave:</label>
+                    <Input
+                        tipo={"checkbox"}
+                        placeholder={"Chave"}
+                        onChange={(e) => setChave(!chave)}
+                        nome={"chave"}
+                    />
+                    {chave && (
+                        <>
+                            <label>Número da chave:</label>
+                            <Input
+                                tipo={"number"}
+                                placeholder={"N° :"}
+                                valor={numerochave}
+                                onChange={(e) => setNumerochave(e.target.value)}
+                                nome={"Número"}
+                            />
+                            {errors.numerochave && <span className="text-red-500">{errors.numerochave}</span>}
+                        </>
+                    )}
+                </div>
+                <div className="w-[70%] m-2">
+                    <label>Projetor:</label>
+                    <Input
+                        tipo={"checkbox"}
+                        placeholder={"projetor"}
+                        onChange={(e) => setProjetor(!projetor)}
+                        nome={"projetor"}
+                    />
+                </div>
+                <div className="w-[70%] m-2">
+                    <label>Chave eletronica:</label>
+                    <Input
+                        tipo={"checkbox"}
+                        placeholder={"chave eletronica"}
+                        onChange={(e) => setChaveeletronica(!chaveeletronica)}
+                        nome={"chaveeletronica"}
+                    />
+                </div>
+                <div className="w-[70%] m-2">
+                    <label>Ventilador:</label>
+                    <Input
+                        tipo={"checkbox"}
+                        placeholder={"ventilador"}
+                        onChange={(e) => setVentilador(!ventilador)}
+                        nome={"ventilador"}
+                    />
+                </div>
 
-                    <SendButton />
-                </form>
-            </div>
+                <SendButton />
+            </form>
         </div>
-    );
+    </div>
+);
 };
 
 export default Ambiente;
