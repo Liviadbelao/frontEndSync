a7d3f67192e1145805c9de9421793ac32a3e28a2
Maria <maria.cancian@aluno.senai.br>
2024-11-11 15:17:13 -0300

    feat: css ambientes adm

--- app/administracao/ambientes/page.jsx
+++ app/administracao/ambientes/page.jsx
@@ -143,15 +143,15 @@ const ambientes = () => {
                     onChange={(e) => setFiltro(e.target.value)}
                     className="border p-2 mb-4"
                 />
-                <div className="grid grid-cols-2 gap-4">
+                <div className="grid grid-cols-4 gap-4 gap-y-20">
                     {ambientesFiltrados && ambientesFiltrados.length > 0 ? (
                         ambientesFiltrados.map((ambiente) => (
 
 
-                            <div className="bg-[#D9D9D9] w-[60%] h-50 rounded-lg z-10 fixed relative mb-10" key={ambiente.numero_ambiente}>
-                                <img src={`http://localhost:3033${ambiente.caminho_imagem}`} className="h-[150px] w-[500px] rounded-lg" alt={ambiente.nome} />
+                            <div className="bg-[#D9D9D9] w-[80%] h-[100%] rounded-lg z-10 fixed relative mb-10" key={ambiente.numero_ambiente}>
+                                <img src={`http://localhost:3033${ambiente.caminho_imagem}`} className="h-[180px] w-[600px] rounded-lg" alt={ambiente.nome} />
                                 <div className="p-4">
-                                    <p className="font-semibold text-xs mb-2 text-black">{ambiente.nome}</p>
+                                    <p className="font-semibold text-xs mb-2 text-black mt-8">{ambiente.nome}</p>
                                     <div className="bg-[#9A1915] w-10 h-[2px] m-auto"></div>
                                     {
                                         ambiente.tipodoambiente === "blocooficina" ? <GiStaplerPneumatic className="w-8 h-8 m-auto text-black " /> : null
@@ -164,7 +164,7 @@ const ambientes = () => {
                                 <div className="absolute top-[53%] left-[50%] transform -translate-x-1/2 -translate-y-1/2">
                                     {
                                         ambiente.disponivel ? (
-                                            <button className="bg-[#9A1915] text-white p-2 rounded-full z-20" onClick={() => reservarAmbiente(ambiente.numero_ambiente)}>
+                                            <button className="bg-[#9A1915] text-white mb-6 p-2 rounded-full z-20" onClick={() => reservarAmbiente(ambiente.numero_ambiente)}>
                                                 Reservar
                                             </button>
                                         ) : (
@@ -172,7 +172,7 @@ const ambientes = () => {
                                         )
                                     }
                                 </div>
-                                <div className={`bg-[#9A1915] gap-2 flex text-white z-20 p-2 rounded-full absolute left-[50%] transform -translate-x-1/2 -translate-y-1/2 ${ambiente.disponivel ? '' : 'bg-[#2e2e2e]'}`}>
+                                <div className={`bg-[#9A1915] gap-2 flex text-white z-20 p-2 mt-9 rounded-full absolute left-[50%] transform -translate-x-1/2 -translate-y-1/2 ${ambiente.disponivel ? '' : 'bg-[#2e2e2e]'}`}>
                                     {
                                         ambiente.chave && <IoKeyOutline />
                                     }
