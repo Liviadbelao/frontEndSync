c3b1345aa891c2f56a5172b9835d3d58fd4fef7b
Liviadbelao <liviabelao.564@gmail.com>
2024-11-27 11:31:23 -0300

    feat: arrumando deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '192.168.69.191',
-        port: '3033',
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/uploads/**',
-      },
-    ],
-  },
-};
-
-export default nextConfig;  // Use export default para módulos ES
+  // reactStrictMode: true, // Ativa o reactStrictMode, que inclui Hot Reload
+ 
+   images: {
+     remotePatterns: [
+       {
+         protocol: 'http',
+         hostname: 'localhost',
+         port: '3033',
+         pathname: '/uploads/**',
+       },
+     ],
+   },
+ };
+ 
+ export default nextConfig;
--- src/config/configApi.js
+++ src/config/configApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-        baseURL: 'https://192.168.69.191:3033',
-        timeout: 50000,
+        baseURL: 'http://localhost:3033',
+        timeout: 5000,
         headers: {'X-Custom-Header': 'foobar'}
 })
