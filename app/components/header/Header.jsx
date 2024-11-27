3075124a6f991796e1f5350539399162f5410945
bela souza <osouza.isabela@gmail.com>
2024-11-27 16:52:34 -0300

    feat: adicionar botão de configuração no cabeçalho

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -8,12 +8,25 @@ const Header = () => {
     router.push("/");
   };
 
+  const handleConfigClick = () => {
+    router.push(`/configuracaoInicial/${nif}`);
+  };
+
   return (
     <div className="flex justify-between items-center w-100 bg-[#D9D9D9] p-0.5 sm:p-2">
       <img className="w-40 sm:w-60" src={"/images/logoSenai/logo.png"} />
-     
-        <img  w-8 h-8 onClick={handleLogout} src={"/images/logoSenai/exit.png"} />
-      
+      <img
+        className="w-5 sm:w-10 ml-[76%] cursor-pointer"
+        src={"/images/logoSenai/engrenagem.png"}
+        onClick={handleConfigClick}
+        alt="Configuração Inicial"
+      />
+      <img
+        className="w-12 h-12 cursor-pointer"
+        src={"/images/logoSenai/exit.png"}
+        onClick={handleLogout}
+        alt="Logout"
+      />
     </div>
   );
 };
