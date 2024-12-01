2bf2ff5b25c3ca13cd0b47d97089253440a65f13
giobasiliox <giovana.basilio@aluno.senai.br>
2024-12-01 17:57:32 -0300

    feat: adicionar seleção de horários na reserva de sala e melhorar estilo do modal

--- app/components/reservaSala/ReservarSala.jsx
+++ app/components/reservaSala/ReservarSala.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Modal from '@mui/material/Modal';
-import Image from 'next/image';
 
 const ReservaSala = ({ onClose, onConfirm, img, name, typeAmb, startTime, setStartTime, endTime, setEndTime }) => {
   const getCurrentTime = () => {
@@ -61,6 +60,10 @@ const ReservaSala = ({ onClose, onConfirm, img, name, typeAmb, startTime, setSta
     onClose();
   };
 
+  const handleShowTimeSelection = () => {
+    setShowTimeSelection(true);
+  };
+
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-10 overflow-hidden">
       <Modal open={true} onClose={handleClose} BackdropProps={{ style: { pointerEvents: 'none' } }}>
@@ -88,14 +91,19 @@ const ReservaSala = ({ onClose, onConfirm, img, name, typeAmb, startTime, setSta
 
           <p className='mt-10 text-black text-xl font-bold text-center'>{name}</p>
           <p className='text-black text-lg mt-4'>Início: {startTime} {typeAmb === 'externo' && `| Fim: ${endTime}`}</p>
-          <button className="rounded-full bg-[#b42424] w-80 mt-5 p-4 px-10 text-white" onClick={onConfirm}>
-            Sim
-          </button>
+          
           {typeAmb === 'externo' && (
-            <button className="rounded-full bg-[#007bff] w-80 p-4 px-10 text-white mt-4" onClick={() => setShowTimeSelection(true)}>
+            <button 
+              className="rounded-full bg-[#b42424]  hover:bg-[#9A1915] transition duration-300 w-60 p-4 px-10 text-white mt-4" 
+              onClick={handleShowTimeSelection} // Exibir opções de horário ao clicar
+            >
               Definir horários
             </button>
           )}
+          
+          <button className="rounded-full bg-[#b42424] w-60 mt-5 p-4 px-10 text-white" onClick={onConfirm}>
+            Sim
+          </button>
         </div>
       </Modal>
 
@@ -103,29 +111,44 @@ const ReservaSala = ({ onClose, onConfirm, img, name, typeAmb, startTime, setSta
         <Modal open={showTimeSelection} onClose={() => setShowTimeSelection(false)} BackdropProps={{ style: { pointerEvents: 'none' } }}>
           <div
             onClick={(e) => e.stopPropagation()}
-            className="flex flex-col justify-center items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#d9d9d9] rounded-[10px] shadow-lg w-[45%] h-[50%] p-16"
+            className="flex flex-col justify-center items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg shadow-xl w-[60%] sm:w-[50%] md:w-[20%] lg:w-[20%] p-8 transition-all ease-in-out duration-300"
           >
-            <p className="text-black text-2xl font-semibold mb-4">Definir horários</p>
-            <input
-              type="time"
-              value={startTime}
-              onChange={handleStartTimeChange}
-              className="rounded-full bg-white w-80 p-4 px-10 text-black m-4"
-            />
-            <input
-              type="time"
-              value={endTime}
-              onChange={handleEndTimeChange}
-              className="rounded-full bg-white w-80 p-4 px-10 text-black m-4"
-            />
-            {timeError && <p className="text-red-500 mt-2">{timeError}</p>}
-            <button
-              className="rounded-full bg-[#28a745] w-80 p-4 px-10 text-white mt-4"
-              onClick={handleConfirmTime}
-              disabled={!startTime || !endTime}
-            >
-              Confirmar horários
-            </button>
+            <p className="text-[#333] text-2xl font-semibold mb-6">Definir horários</p>
+
+            {/* Início */}
+            <div className="w-1/2 mb-4">
+              <p className="text-[#555] text-center text-lg mb-2">Início</p>
+              <input
+                type="time"
+                value={startTime}
+                onChange={handleStartTimeChange}
+                className="rounded-lg bg-[#f0f0f0] w-full p-3 text-[#333] border border-[#ccc] focus:ring-2 focus:ring-[#007bff] transition duration-300"
+              />
+            </div>
+
+            {/* Fim */}
+            <div className="w-1/2 mb-6">
+              <p className="text-[#555] text-center text-lg mb-2">Fim</p>
+              <input
+                type="time"
+                value={endTime}
+                onChange={handleEndTimeChange}
+                className="rounded-lg bg-[#f0f0f0] w-full p-3 text-[#333] border border-[#ccc] focus:ring-2 focus:ring-[#z] transition duration-300"
+              />
+            </div>
+
+            {/* Erro de horário */}
+            {timeError && <p className="text-red-500 text-sm mt-2">{timeError}</p>}
+
+            <div className="flex justify-center w-full mt-4">
+              <button
+                className="rounded-full bg-[#b42424] w-60 py-3 text-white font-semibold hover:bg-[#9A1915] transition duration-300 disabled:bg-[#ccc]"
+                onClick={handleConfirmTime}
+                disabled={!startTime || !endTime}
+              >
+                Confirmar horários
+              </button>
+            </div>
           </div>
         </Modal>
       )}
